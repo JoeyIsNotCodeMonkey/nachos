5af238b99b5d2888c11027c60ed940f41087afbd
JoeyIsNotCodeMonkey <Joey@192.168.2.71>
2016-11-05 16:36:17 -0400

    testdriver

--- nachos/kernel/userprog/test/testDriver.java
+++ nachos/kernel/userprog/test/testDriver.java
@@ -78,6 +78,9 @@ public class testDriver implements Runnable {
 	space.initRegisters();		// set the initial register values
 	space.restoreState();		// load page table register
 
+	
+	CPU.writeRegister(4, 10-space.getSpaceID());
+	
 	CPU.runUserCode();			// jump to the user progam
 	Debug.ASSERT(false);		// machine->Run never returns;
 	// the address space exits
