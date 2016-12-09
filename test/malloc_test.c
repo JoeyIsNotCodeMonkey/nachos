36f0d56deb1a6256d5b396e7ec67abe615f29112
JoeyIsNotCodeMonkey <Joey@192.168.2.71>
2016-12-09 17:03:34 -0500

    aaaa

--- test/malloc_test.c
+++ test/malloc_test.c
@@ -5,24 +5,30 @@ int main(int argc, char const *argv[])
 	int *addr1 = sf_malloc(128);
 	printAddress(addr1);
 
+	char *ptr = (char *)addr1 + 16;
+	*ptr = 'a';
+
 	int a = 1;
 
 	int *addr2 = sf_malloc(256);
 	printAddress(addr2);
 
 	sf_malloc(512);
-	
+
 	sf_malloc(512);
-	
+
 	sf_malloc(512);
-	
+
 	sf_free(addr2);
 	//printf("After free, firstfree size is: %d\n", firstfree->size);
 	sf_malloc(512);
 	//printf("After free, firstfree->next size is: %d\n", firstfree->next->size);
+	sf_malloc(512);
 	printFreeList();
 
-	
+	char b = *ptr;
+
+
 
 
 	//Write("After free, firstfree->next size is: ", 50, 100);
